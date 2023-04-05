d591166ed86dfb231bba1f89dd9df4f85cf586ab
Hapid Fadli <hapidzfadli@gmail.com>
2023-04-05 21:43:54 +0700

    membuat navbar

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-transparant fixed-top">
+    <a class="navbar-brand" href="#">Netflix</a>
+    
+    <div class="collapse navbar-collapse ml-lg-1" id="navbarNav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active mx-2">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item mx-2">
+          <a class="nav-link" href="#">TV Shows</a>
+        </li>
+        <li class="nav-item mx-2">
+          <a class="nav-link" href="#">Movies</a>
+        </li>
+        <li class="nav-item mx-2">
+          <a class="nav-link" href="#">My List</a>
+        </li>
+      </ul>
+    </div>
+    <form class="search-form">
+        <div class="input-group rounded-pill">
+          <input type="text" class="form-control rounded-pill" placeholder="Search">
+          <div class="input-group-append" >
+            <button class="btn btn-transparant" style="margin-left: -40px" type="submit">
+              <i class="fas fa-search" style="color: black;"></i>
+            </button>
+          </div>
+        </div>
+      </form>      
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    
+  </nav>
+  
