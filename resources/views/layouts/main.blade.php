3bf8c045c217046b5bbe9f2430d64d02b9cd2da4
Hapid Fadli <hapidzfadli@gmail.com>
2023-04-12 05:08:47 +0700

    Content-Security-Policy

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="description" content="{{$meta_description}}">
     <meta name="keywords" content="{{$meta_keywords}}">
