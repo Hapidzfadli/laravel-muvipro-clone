628c2036990326a1e6b11f5c8b69f8c2fcbac284
Hapid Fadli <hapidzfadli@gmail.com>
2023-04-12 10:07:30 +0700

    remove ssl

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="description" content="{{$meta_description}}">
+    <meta http-equiv="Content-Security-Policy" content="img-src http:">
     <meta name="keywords" content="{{$meta_keywords}}">
     <meta name="google-site-verification" content="nyg9sngXbMez9cTZcdhVhat7E2INzl1Igom4oM02yJo" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
