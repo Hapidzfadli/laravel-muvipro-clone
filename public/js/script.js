4f11ee9f2337e34cc72a5c0818085184358cdba9
Hapid Fadli <hapidzfadli@gmail.com>
2023-04-15 05:55:51 +0700

    menambah efek untuk navbar

--- public/js/script.js
+++ public/js/script.js
@@ -14,3 +14,17 @@ $(document).ready(function() {
         $('.player-number').text(intVal)
     });
 });
+
+
+$(document).ready(function() {
+  var prevScrollpos = window.pageYOffset;
+  $(window).scroll(function() {
+    var currentScrollpos = window.pageYOffset;
+    if (prevScrollpos > currentScrollpos) {
+      $(".navbar").removeClass("show");
+    } else {
+      $(".navbar").addClass("show");
+    }
+    prevScrollpos = currentScrollpos;
+  });
+});
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,7 @@
+<!-- Histats.com  START (hidden counter) -->
+<a href="/" alt="web stats" target="_blank" >
+<img  src="//sstatic1.histats.com/0.gif?4622821&101" alt="web stats" border="0">
+<!-- Histats.com  END  -->
 <footer class="mx-auto">
     <p>&#169; Copyright Indoseries</p>
 </footer>
